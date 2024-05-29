66b425bc30ff5b61361752453e1590f52257bab3
Madhav Shekhar Sharma <33770878+mdvsh@users.noreply.github.com>
2024-05-29 22:35:34 +0530

    vec3, color utils; test ppm gen, bench against cout, todo: ray class

---
+++ src/color.h
@@ -0,0 +1,25 @@
+//
+// Created by Madhav Shekhar Sharma on 5/29/24.
+//
+
+#ifndef RTC_SRC_COLOR_H_
+#define RTC_SRC_COLOR_H_
+
+#include "vec3.h"
+
+using color = vec3;
+
+void print_color(std::ostream &out, const color &pixel_color) {
+  double r = pixel_color.x();
+  double g = pixel_color.y();
+  double b = pixel_color.z();
+
+  // Translate the [0,1] comp values within the byte range [0,255]
+  int r_b = int(255.999 * r);
+  int g_b = int(255.999 * g);
+  int b_b = int(255.999 * b);
+
+  out << r_b << ' ' << g_b << ' ' << b_b << '\n';
+}
+
+#endif//RTC_SRC_COLOR_H_
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <sstream>
+
+#include "color.h"
+#include "vec3.h"
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+  const int image_width = 256;
+  const int image_height = 256;
+
+  std::stringstream ss;
+  ss << "P3\n"
+	 << image_width << ' ' << image_height << "\n255\n";
+
+  for (int j = 0; j < image_height; j++) {
+	std::clog << "\rScanlines remaining: " << (image_height - j) << " " << std::flush;
+	for (int i = 0; i < image_width; i++) {
+	  double r = double(i) / (image_width - 1);
+	  double g = double(j) / (image_height - 1);
+	  double b = 0.0;
+
+	  color pixel_color{r, g, b};
+	  print_color(ss, pixel_color);
+	}
+  }
+
+  std::clog << "\rDone.                 \n";
+  std::cout << ss.str();
 }
